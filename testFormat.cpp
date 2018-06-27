a46ea50f520f1d0ae387e2c6624b4b1689304b55
shantanutrip <shantanutripathi237@yahoo.co.in>
2018-06-27 20:48:29 +0530

    format

---
+++ testFormat.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**bits/stdc++.h is a header file which also includes all other standard libraries and stl header files.
+* In future, it will help you a lot
+*/
+//Codechef tends to give number of test cases. 
+//Variable 't' takes care of the number of test cases
+int main(){
+	Boost;
+	int t; 
+	cin>>t;
+	while(t--){
+		//your code
+	}
+	return 0;
+}
